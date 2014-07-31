f59e95b6674e458e505705f91c251e5c49761cb9
Kate Harrison <harriska@eecs.berkeley.edu>
2014-07-30 20:56:58 -0700

    Added documentation of groups. Added *.pyc to .gitignore.

--- documentation/document_all.py
+++ documentation/document_all.py
@@ -2,11 +2,19 @@
 import os
 import subprocess
 
+# See http://mainlydata.kubadev.com/python/sphinx-how-to-make-autodoc-really-automatic/
+
 source_directories = [os.path.abspath("..")]
 rst_directory = "."
 
 excluded_modules = ["test", "test2", "test3", "doc_inherit", "snippets",
                     "doc_inherit2", "custom_logging", "see_profile"]
+
+# groups = ["propagation_model", "protected_entities", "protected_entity", "region", "ruleset"]
+groups = [("Propagation models", "propagation_model"), ("Protected entities", "protected_entities"),
+          ("Protected entity", "protected_entity"), ("Region", "region"), ("Ruleset", "ruleset")]
+
+
 test_suffix = "test.py"
 
 modules = []
@@ -48,6 +56,55 @@ Indices and tables
         f.write(output)
 
 
+def generate_group_rst(group_name, group_str, module_name_list):
+    overline = "".center(len(group_name), "#")
+    module_name_list_str = ""
+    for module_name in module_name_list:
+        module_name_list_str += "   %s\n" % module_name
+    glob_module_name_list_str = ""
+    for module_name in module_name_list:
+        glob_module_name_list_str += "   %s\n" % module_name
+    output = """
+%s
+%s
+%s
+
+
+Heritage
+========
+.. inheritance-diagram:: %s
+
+Modules
+=======
+Links point to the module's individual page.
+
+.. autosummary::
+
+%s
+
+""" % (overline, group_str, overline, module_name_list_str, glob_module_name_list_str)
+
+    for module_name in module_name_list:
+        underline = "".center(len(module_name), '-')
+        output += """
+%s
+%s
+
+.. automodule:: %s
+   :members:
+""" % (module_name, underline, module_name)
+
+    output += """
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`"""
+
+    with open(os.path.join(rst_directory, "group_" + group_name + ".rst"), "w") as f:
+        f.write(output)
+
 
 for src_dir in source_directories:
     files = os.listdir(src_dir)
@@ -67,11 +124,27 @@ for src_dir in source_directories:
         rst_string = generate_rst(module_name)
         modules.append(module_name)
 
+print("All modules:", modules)
+for (group_str, group_name) in groups:
+    print("Adding group %s" % group_name)
+    module_name_list = []
+    for module_name in modules:
+        if module_name.startswith(group_name):
+            # print("Found!")
+            module_name_list.append(module_name)
+    print("  Modules: %s" % str(module_name_list))
+    generate_group_rst(group_name, group_str, module_name_list)
+
+
 
 module_str = ""
 for module in modules:
     module_str += "   %s\n" % module
 module_csv = " ".join(modules)
+
+group_str = ""
+for (_, group_name) in groups:
+    group_str += "   group_%s\n" % group_name
 
 index_output = """
 .. Whitespace Evaluation SofTware documentation master file, created by
@@ -88,6 +161,15 @@ Inheritance diagrams
 .. inheritance-diagram:: %s
    :parts: 1
 
+
+Groups:
+=======
+.. toctree::
+   :maxdepth: 1
+   :titlesonly:
+
+%s
+
 Modules:
 =========
 .. toctree::
@@ -102,7 +184,7 @@ Indices and tables
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
-""" % (module_csv, module_str)
+""" % (module_csv, group_str, module_str)
 
 print("Documented the following modules:\n%s" % module_str)
 
